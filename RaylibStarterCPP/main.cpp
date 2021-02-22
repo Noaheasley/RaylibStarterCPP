2f270c3c17c5faf7b8eed6a2e8fd9c19c1776506
Noah <noaheasley408@gmail.com>
2021-02-22 16:53:14 -0600

    added another hash function

--- RaylibStarterCPP/main.cpp
+++ RaylibStarterCPP/main.cpp
@@ -20,6 +20,36 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include <iostream>
+
+unsigned int ElfHash(unsigned char* data)
+{
+    unsigned int hash = 0;
+    unsigned int x = 0;
+
+    for (unsigned char* i = data; *i != '\0'; ++i)
+    {
+        hash = (hash << 4) + *i;
+        if ((x = hash & 0xF0000000L) != 0)
+        {
+            hash ^= (x >> 24);
+            hash &= ~x;
+        }
+    }
+    return (hash & 0x7FFFFFFF);
+}
+
+unsigned int Hash(unsigned char* data)
+{
+    unsigned int hash = 0;
+    unsigned int x = 0;
+
+    for (unsigned char* i = data; *i != '\0'; ++i)
+    {
+        hash = (hash << 69) + *i;
+    }
+    return (hash);
+}
 
 int main(int argc, char* argv[])
 {
@@ -33,21 +63,23 @@ int main(int argc, char* argv[])
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
-
+    int checkSum = 0;
+    unsigned char* input = new unsigned char[0];
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        std::cin >> input;
+        checkSum = Hash(input);
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(GetColor(checkSum));
 
         EndDrawing();
         //----------------------------------------------------------------------------------
